298cb9b5f93a07a2124ae42c3e0bcbee000ed3f6
Michal <238181@student.pwr.edu.pl>
2017-11-27 05:20:43 +0100

    Polishing

--- src/main/java/me/mjaroszewicz/controllers/RegistrationController.java
+++ src/main/java/me/mjaroszewicz/controllers/RegistrationController.java
@@ -2,26 +2,19 @@ package me.mjaroszewicz.controllers;
 
 
 import me.mjaroszewicz.dtos.UserDto;
-import me.mjaroszewicz.entities.User;
 import me.mjaroszewicz.exceptions.RegistrationException;
 import me.mjaroszewicz.services.SecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
-import javax.validation.executable.ValidateOnExecution;
 
 @Controller
 public class RegistrationController {
@@ -30,13 +23,6 @@ public class RegistrationController {
 
     @Autowired
     private SecurityService securityService;
-//
-//    @GetMapping("/user/registration")
-//    public String getRegistratioNForm(WebRequest request, Model mdl){
-//        UserDto userDto = new UserDto();
-//        mdl.addAttribute("user", userDto);
-//        return "registration";
-//    }
 
     @PostMapping("/user/registration")
     public ResponseEntity<String> registerUserAccount(@RequestBody UserDto userDto, BindingResult result, WebRequest request, Errors err){
